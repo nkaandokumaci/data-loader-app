392467219890146a1e08ec9d5dac9732a47add74
kaan_ <kaan_@LAPTOP-BITC222J>
2023-08-07 14:34:29 +0300

    Add project files.

---
+++ dataloaders/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace dataloaders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FileName = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FileName
+            // 
+            this.FileName.Location = new System.Drawing.Point(966, 172);
+            this.FileName.Name = "FileName";
+            this.FileName.Size = new System.Drawing.Size(200, 56);
+            this.FileName.TabIndex = 1;
+            this.FileName.Text = "Browse File";
+            this.FileName.UseVisualStyleBackColor = true;
+            this.FileName.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar1.Location = new System.Drawing.Point(59, 607);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1107, 52);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(59, 408);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(300, 31);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDown1_KeyPress);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(510, 408);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(300, 31);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            this.numericUpDown2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDown2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 349);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 37);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "MDL RT NO :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(503, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 37);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "CHANNEL NO :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 536);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(316, 37);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "LOADING STATUS :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(64, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 37);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "BINARY FILE :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(52, 829);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(378, 37);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "BITES OFP Data Loader:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 172);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(834, 56);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Button1
+            // 
+            this.Button1.BackgroundImage = global::dataloaders.Properties.Resources.sssssss;
+            this.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button1.Location = new System.Drawing.Point(1219, 172);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(483, 401);
+            this.Button1.TabIndex = 20;
+            this.Button1.Text = "Download";
+            this.Button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(52, 709);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 42);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "label7";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(449, 529);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(218, 44);
+            this.textBox2.TabIndex = 22;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(472, 829);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(361, 37);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Nevzat Kaan Dokumaci ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::dataloaders.Properties.Resources.mmmm;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1762, 1053);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.FileName);
+            this.Name = "Form1";
+            this.Text = "Data Loader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button FileName;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ dataloaders/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace dataloaders
+{
+    public partial class Form1 : Form
+    {
+        private DateTime programStartTime;
+        private Timer timer;
+        private int progressValue;
+        private int downloadPercentage;
+        private bool fileSelected = false;
+        private bool isDownloading = false;
+        private string selectedFilePath = "";
+
+
+        public int DownloadPercentage
+        {
+            get { return downloadPercentage; }
+            set
+            {
+                downloadPercentage = value;
+                progressBar1.Value = value;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimer();
+            //timer.Tick += Timer_Tick;
+            //timer = new Timer();
+            //timer.Interval = 1000;
+            //timer.Tick += Timer_Tick;
+            
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Set the filter and other options for the OpenFileDialog
+                openFileDialog.Filter = "Binary Files (*.bin)|*.bin";
+                openFileDialog.Title = "Select a .bin file";
+
+                // Show the OpenFileDialog and check if the user clicked the OK button
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Get the selected file path and display it in the TextBox
+                    selectedFilePath = openFileDialog.FileName;
+                    textBox1.Text = selectedFilePath;
+
+                    // Set the fileSelected flag to true
+                    fileSelected = true;
+                }
+            }
+            /*string fileUrl = textBox2.Text.Trim();
+
+            if (!string.IsNullOrEmpty(fileUrl))
+            {
+                try
+                {
+                  
+                    UpdateLoadingStatus();
+                    isDownloading = true;
+
+                    using (WebClient webClient = new WebClient())
+                    {
+                        string fileName = Path.GetFileName(fileUrl);
+
+                        using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                        {
+                            saveFileDialog.FileName = fileName;
+                            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                            {
+                                await webClient.DownloadFileTaskAsync(new Uri(fileUrl), saveFileDialog.FileName);
+
+                                MessageBox.Show("File downloaded successfully!");
+                                UpdateLoadingStatus() ;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error occurred while downloading the file: " + ex.Message);
+                }
+                finally
+                {
+                    isDownloading = false;
+                  
+                }
+            }*/
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Record the start time of the program
+            programStartTime = DateTime.Now;
+
+            // Start the timer to update the elapsed time
+            timer.Start();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 100; // Set the interval in milliseconds (adjust as needed)
+            timer.Tick += Timer_Tick;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Update the ProgressBar value based on the progressValue
+            progressBar1.Value = progressValue;
+
+            // Check if the progress reaches 100%
+            if (progressValue >= progressBar1.Maximum)
+            {
+                // Stop the timer
+                timer.Stop();
+
+                // Reset the progressValue for future use
+                progressValue = 0;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            // Clear the NumericUpDown control if the entered value is not numeric
+            if (!decimal.TryParse(numericUpDown1.Text, out decimal value))
+            {
+                numericUpDown1.Text = string.Empty;
+            }
+
+        }
+
+        private void numericUpDown1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow numeric characters and control keys (e.g., Backspace, Delete)
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // Mark the event as handled to suppress the key press
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow numeric characters and control keys (e.g., Backspace, Delete)
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // Mark the event as handled to suppress the key press
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void UpdateLoadingStatus(string status)
+        {
+            textBox2.Text = status;
+        }
+        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Update the ProgressBar based on the download percentage
+            progressBar1.Value = e.ProgressPercentage;
+        }
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Calculate the elapsed time since the program started
+            TimeSpan elapsedTime = DateTime.Now - programStartTime;
+
+            // Update the Label control with the elapsed time
+            label7.Text = string.Format("Elapsed Time: {0:D2}:{1:D2}:{2:D2}",
+                elapsedTime.Hours, elapsedTime.Minutes, elapsedTime.Seconds);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Button1_Click_1(object sender, EventArgs e)
+        {
+            // Check if a file has been selected and no download process is ongoing
+            if (!fileSelected || isDownloading)
+            {
+                MessageBox.Show("Please select a file to download.");
+                return;
+            }
+
+            // Set isDownloading to true before the download starts
+            isDownloading = true;
+
+            try
+            {
+                UpdateLoadingStatus("Downloading...");
+                progressBar1.Value = 0; // Reset the progress bar
+
+                using (WebClient webClient = new WebClient())
+                {
+                    // Get the file name from the URL
+                    string fileName = Path.GetFileName(selectedFilePath);
+
+                    using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                    {
+                        saveFileDialog.FileName = fileName;
+                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            // Download the file and save it to the destination path
+                            webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
+                            await webClient.DownloadFileTaskAsync(new Uri(selectedFilePath), saveFileDialog.FileName);
+
+                            MessageBox.Show("File downloaded successfully!");
+                            UpdateLoadingStatus("Downloaded");
+                        }
+                        else
+                        {
+                            // User canceled the save dialog, cancel the download
+                            webClient.CancelAsync();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred while downloading the file: " + ex.Message);
+            }
+            finally
+            {
+                // Download is complete, update the status
+                isDownloading = false;
+                fileSelected = false; // Reset the fileSelected flag
+                progressBar1.Value = 0; // Reset the progress bar
+            }
+
+        }
+
+        /*private void progressBar1_Click(object sender, EventArgs e)
+        {
+            // Check if the event arguments are of type DownloadProgressChangedEventArgs
+            if (e is DownloadProgressChangedEventArgs downloadArgs)
+            {
+                // Update the ProgressBar based on the download percentage
+                progressBar1.Value = downloadArgs.ProgressPercentage;
+            }
+        }*/
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private async void button2_Click(object sender, EventArgs e)
+        {
+            // Check if a file has been previously selected
+            if (string.IsNullOrEmpty(selectedFilePath))
+            {
+                MessageBox.Show("Please select a file using the 'Browse' button.");
+                return;
+            }
+
+            // Proceed with the download using the previously selected file path
+            try
+            {
+                isDownloading = true; // Set isDownloading to true before the download starts
+                UpdateLoadingStatus();
+                //progressBar1.Value = 0; // Reset the progress bar
+
+                using (WebClient webClient = new WebClient())
+                {
+                    string fileName = Path.GetFileName(selectedFilePath);
+
+                    using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                    {
+                        saveFileDialog.FileName = fileName;
+                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            // Download the file and save it to the destination path
+                            webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
+                            await webClient.DownloadFileTaskAsync(new Uri(selectedFilePath), saveFileDialog.FileName);
+
+
+                            MessageBox.Show("File downloaded successfully!");
+                            //isDownloading = false; // Set isDownloading to false when download is complete
+                            //UpdateLoadingStatus();
+                            //progressBar1.Value = 0; // Reset the progress bar
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error occurred while downloading the file: " + ex.Message);
+            }
+            finally
+            {
+                // Download is complete, update the status
+                isDownloading = false;
+                UpdateLoadingStatus();
+                progressBar1.Value = 0; // Reset the progress bar
+            }
+        }*/
+        //private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        //{
+        // Update the ProgressBar based on the download percentage
+        //  progressBar1.Value = e.ProgressPercentage;
+        //}
+    }
+}
